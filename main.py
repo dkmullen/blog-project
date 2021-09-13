fa8c2f055e7f1cd68585be410392e16f09fd435a
dennis-mullen-knoxco <dennis.mullen@knoxcounty.org>
2021-09-13 10:29:44 -0400

    Make form work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 app = Flask(__name__)
 all_posts = []
@@ -7,7 +8,7 @@ all_posts = []
 
 @app.route('/')
 def home():
-    global  all_posts
+    global all_posts
     response = requests.get("https://api.npoint.io/e42b353ee387383898c7")
     all_posts = response.json()
     print(all_posts)
@@ -19,11 +20,6 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-
-
 @app.route('/<int:post_id>')
 def get_post(post_id):
     requested_post = None
@@ -32,6 +28,27 @@ def get_post(post_id):
             requested_post = post
             print(requested_post)
     return render_template("post.html", post=requested_post)
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        if request.form['name'] and request.form['email'] and request.form['phone'] and request.form['message']:
+            event=f"Name: {request.form['name']}\nEmail: {request.form['email']}\nMessage: {request.form['message']}"
+            send_email(event)
+            return render_template("contact.html", message_sent=f"Hey now, {request.form['name']}")
+    return render_template("contact.html", message_sent=False)
+
+
+def send_email(event):
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user="", password="")
+        connection.sendmail(
+            from_addr="",
+            to_addrs="",
+            msg=f"Subject: Contact Form Message\n\n{event}"
+            )
 
 
 if __name__ == "__main__":
